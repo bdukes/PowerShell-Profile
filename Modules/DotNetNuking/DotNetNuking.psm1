b80e2e11cf271b551b73425f993e08e9fe5af0a3
Brian Dukes <bdukes@engagesoftware.com>
2013-01-16 15:10:59 -0600

    Output what the script is doing

--- Modules/DotNetNuking/DotNetNuking.psm1
+++ Modules/DotNetNuking/DotNetNuking.psm1
@@ -110,41 +110,53 @@ function New-DotNetNukeSite {
   $majorVersion = $v.Major
   $formattedVersion = $v.Major.ToString('0#') + '.' + $v.Minor.ToString('0#') + '.' + $v.Build.ToString('0#')
   if ($includeSource -eq $true) {
+    Write-Host "Extracting DNN $formattedVersion source"
     &7za x -oC:\inetpub\wwwroot\$siteName "${env:soft}\DNN\Versions\DotNetNuke $majorVersion\DotNetNuke_Community_${formattedVersion}_Source.zip" | Out-Null
+    Write-Host "Copying DNN $formattedVersion source symbols into install directory"
     cp "${env:soft}\DNN\Versions\DotNetNuke $majorVersion\DotNetNuke_Community_${formattedVersion}_Symbols.zip" C:\inetpub\wwwroot\$siteName\Website\Install\Module
+    Write-Host "Updating site URL in sln files"
     ls C:\inetpub\wwwroot\$siteName\*.sln | % { Set-Content $_ ((Get-Content $_) -replace '"http://localhost/DotNetNuke_Community"', "`"http://$siteName`"") }
   }
   
   if ($siteZip -eq $null) { 
     $siteZip = "${env:soft}\DNN\Versions\DotNetNuke $majorVersion\DotNetNuke_Community_${formattedVersion}_Install.zip"
   }
+  Write-Host "Extracting DNN site"
   &7za x -y -oC:\inetpub\wwwroot\$siteName\Website $siteZip | Out-Null
 
+  Write-Host "Creating HOSTS file entry for $siteName"
   Add-HostFileEntry $siteName
 
+  Write-Host "Creating IIS app pool"
   New-WebAppPool $siteName
+  Write-Host "Creating IIS site"
   New-Website $siteName -HostHeader $siteName -PhysicalPath C:\inetpub\wwwroot\$siteName\Website -ApplicationPool $siteName
   # TODO: Setup SSL cert & binding
 
+  Write-Host "Setting modify permission on website files for IIS AppPool\$siteName"
   Set-ModifyPermission C:\inetpub\wwwroot\$siteName\Website $siteName
   
   [xml]$webConfig = Get-Content C:\inetpub\wwwroot\$siteName\Website\web.config
   if ($databaseBackup -eq $null) {
+    Write-Host "Creating new database"
     New-DotNetNukeDatabase $siteName
   }
   else {
+    Write-Host "Restoring database"
     Restore-DotNetNukeDatabase $siteName $databaseBackup
 
     $objectQualifier = $webConfig.configuration.dotnetnuke.data.providers.add.objectQualifier.TrimEnd('_')
     $databaseOwner = $webConfig.configuration.dotnetnuke.data.providers.add.databaseOwner.TrimEnd('.')
 
     if ($oldDomain -ne $null) {
+      Write-Host "Updating portal aliases"
       Invoke-Sqlcmd -Query "UPDATE $(Get-DotNetNukeDatabaseObjectName 'PortalAlias' $databaseOwner $objectQualifier) SET HTTPAlias = REPLACE(HTTPAlias, '$oldDomain', '$siteName')" -Database $siteName
       Invoke-Sqlcmd -Query "UPDATE $(Get-DotNetNukeDatabaseObjectName 'PortalSettings' $databaseOwner $objectQualifier) SET SettingValue = REPLACE(SettingValue, '$oldDomain', '$siteName') WHERE SettingName = 'DefaultPortalAlias'" -Database $siteName
       # TODO: Update remaining .com aliases to .com.dev
       # TODO: Add all aliases to host file and IIS
     }
 
+    Write-Host "Setting SMTP to localhost"
     Invoke-Sqlcmd -Query "UPDATE $(Get-DotNetNukeDatabaseObjectName 'HostSettings' $databaseOwner $objectQualifier) SET SettingValue = 'localhost' WHERE SettingName = 'SMTPServer'" -Database $siteName
     Invoke-Sqlcmd -Query "UPDATE $(Get-DotNetNukeDatabaseObjectName 'HostSettings' $databaseOwner $objectQualifier) SET SettingValue = '0' WHERE SettingName = 'SMTPAuthentication'" -Database $siteName
     Invoke-Sqlcmd -Query "UPDATE $(Get-DotNetNukeDatabaseObjectName 'HostSettings' $databaseOwner $objectQualifier) SET SettingValue = 'N' WHERE SettingName = 'SMTPEnableSSL'" -Database $siteName
@@ -157,12 +169,18 @@ function New-DotNetNukeSite {
   $webConfig.configuration.appSettings.add | ? { $_.key -eq 'SiteSqlServer' } | % { $_.value = $connectionString }
   $webConfig.configuration.dotnetnuke.data.providers.add.objectQualifier = $objectQualifier
   $webConfig.configuration.dotnetnuke.data.providers.add.databaseOwner = $databaseOwner
+  Write-Host "Updating web.config with connection string and data provider attributes"
   $webConfig.Save("C:\inetpub\wwwroot\$siteName\Website\web.config")
   
+
+  Write-Host "Creating SQL Server login for IIS AppPool\$siteName"
   Invoke-Sqlcmd -Query "CREATE LOGIN [IIS AppPool\$siteName] FROM WINDOWS WITH DEFAULT_DATABASE = [$siteName];" -Database master
+  Write-Host "Creating SQL Server user"
   Invoke-Sqlcmd -Query "CREATE USER [IIS AppPool\$siteName] FOR LOGIN [IIS AppPool\$siteName];" -Database $siteName
+  Write-Host "Adding SQL Server user to db_owner role"
   Invoke-Sqlcmd -Query "EXEC sp_addrolemember N'db_owner', N'IIS AppPool\$siteName';" -Database $siteName
   
+  Write-Host "Launching http://$siteName"
   Start-Process -FilePath http://$siteName
 
 <#
