d293a1d58bd2ebc6be72f330de67b3a639b7bcff
Brian Dukes <bdukes@engagesoftware.com>
2013-01-18 10:18:00 -0600

    Update TFS binding

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -12,5 +12,10 @@ function Set-ModifyPermission ($directory, $username, $domain = 'IIS APPPOOL') {
 }
 
 function GitTfs-Clone ($tfsPath, $gitPath) {
-    git tfs clone http://tfs2010.etg-inc.net:8080/tfs/Engage%20TFS%202010 "$tfsPath" "$gitPath"
+    git tfs clone http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010 "$tfsPath" "$gitPath"
+}
+
+function Fix-GitTfsBindings () {
+    git config tfs-remote.default.legacy-urls http://tfs2010.etg-inc.net:8080/tfs
+    git config tfs-remote.default.url http://tfs.etg-inc.net:8080/tfs
 }
