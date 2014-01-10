9bb6aae5fcedd794d06cefa15a1fb0912de5d8c4
Brian Dukes <bdukes@engagesoftware.com>
2014-01-10 15:02:40 -0600

    Remove BrowserStack command
    Local tunnelling is now done through browser extensions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -68,19 +68,6 @@ function Fix-GitTfsBindings () {
     git config tfs-remote.default.url http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010
 }
 
-function Open-BrowserstackTunnel {
-    param(
-        [parameter(Mandatory=$true,position=0)]$hostName, 
-        $port = 80, 
-        $ssl = $false);
-
-    $sslBit = 0
-    if ($ssl) { $sslBit = 1 }
-
-    Write-Verbose "java -jar c:\tools\BrowserStackTunnel.jar $env:browserStackTunnelKey `"$hostName,$port,$sslBit`""
-    java -jar c:\tools\BrowserStackTunnel.jar $env:browserStackTunnelKey "$hostName,$port,$sslBit"
-}
-
 Import-Module posh-git
 
 # Set up a simple prompt, adding the git prompt parts inside git repos
