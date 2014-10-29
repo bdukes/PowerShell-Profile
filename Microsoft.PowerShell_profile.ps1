51b82d22c14f133b8f593278822ae032651c1df8
Brian Dukes <bdukes@engagesoftware.com>
2014-10-29 17:57:44 -0500

    Add extra key bindings for PSReadLine

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -90,3 +90,29 @@ function prompt {
 }
 
 Enable-GitColors
+
+<#
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Chord 'Oem7','Shift+Oem7' `
+                         -BriefDescription SmartInsertQuote `
+                         -LongDescription "Insert paired quotes if not already on a quote" `
+                         -ScriptBlock {
+    param($key, $arg)
+
+    $line = $null
+    $cursor = $null
+    [PSConsoleUtilities.PSConsoleReadline]::GetBufferState([ref]$line, [ref]$cursor)
+
+    if ($line[$cursor] -eq $key.KeyChar) {
+        # Just move the cursor
+        [PSConsoleUtilities.PSConsoleReadline]::SetCursorPosition($cursor + 1)
+    }
+    else {
+        # Insert matching quotes, move cursor to be in between the quotes
+        [PSConsoleUtilities.PSConsoleReadline]::Insert("$($key.KeyChar)" * 2)
+        [PSConsoleUtilities.PSConsoleReadline]::GetBufferState([ref]$line, [ref]$cursor)
+        [PSConsoleUtilities.PSConsoleReadline]::SetCursorPosition($cursor - 1)
+    }
+}
+#>
