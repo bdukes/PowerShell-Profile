16ce9b39b95a8cb1acd1d7b967ab70b8c767d231
Brian Dukes <bdukes@engagesoftware.com>
2014-12-17 13:26:47 -0800

    PSReadLine config
    bash style completion
    Remove smart quote completion

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -91,28 +91,6 @@ function prompt {
 
 Enable-GitColors
 
-<#
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadlineKeyHandler -Chord 'Oem7','Shift+Oem7' `
-                         -BriefDescription SmartInsertQuote `
-                         -LongDescription "Insert paired quotes if not already on a quote" `
-                         -ScriptBlock {
-    param($key, $arg)
-
-    $line = $null
-    $cursor = $null
-    [PSConsoleUtilities.PSConsoleReadline]::GetBufferState([ref]$line, [ref]$cursor)
-
-    if ($line[$cursor] -eq $key.KeyChar) {
-        # Just move the cursor
-        [PSConsoleUtilities.PSConsoleReadline]::SetCursorPosition($cursor + 1)
-    }
-    else {
-        # Insert matching quotes, move cursor to be in between the quotes
-        [PSConsoleUtilities.PSConsoleReadline]::Insert("$($key.KeyChar)" * 2)
-        [PSConsoleUtilities.PSConsoleReadline]::GetBufferState([ref]$line, [ref]$cursor)
-        [PSConsoleUtilities.PSConsoleReadline]::SetCursorPosition($cursor - 1)
-    }
-}
-#>
