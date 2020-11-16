23d4001aa52fac36e1c4a5b43e49a087b4db308b
Brian Dukes <bdukes@engagesoftware.com>
2020-11-16 13:26:02 -0600

    Formatting

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,21 +1,21 @@
 #Requires -Version 3
 #Set-StrictMode -Version Latest
 
-Import-Module oh-my-posh
-$ThemeSettings.MyThemesLocation = $PSScriptRoot
-Set-Theme My-Posh-Theme
-[ScriptBlock]$Prompt = $function:prompt
+Import-Module oh-my-posh;
+$ThemeSettings.MyThemesLocation = $PSScriptRoot;
+Set-Theme My-Posh-Theme;
+[ScriptBlock]$Prompt = $function:prompt;
 
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key Tab -Function Complete;
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward;
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward;
 
-#Import-Module Pscx -arg $PSScriptRoot\Pscx.UserPreferences.ps1
-#Set-Alias sudo Invoke-Elevated
-Set-Alias rm Remove-ItemSafely -Option AllScope
+#Import-Module Pscx -arg $PSScriptRoot\Pscx.UserPreferences.ps1;
+#Set-Alias sudo Invoke-Elevated;
+Set-Alias rm Remove-ItemSafely -Option AllScope;
 
-$env:Platform = 'Any CPU'
-$studioInstance = Get-VSSetupInstance -All | Select-VSSetupInstance -Require 'Microsoft.VisualStudio.Workload.NetWeb' -Version 15.0 -Latest
+$env:Platform = 'Any CPU';
+$studioInstance = Get-VSSetupInstance -All | Select-VSSetupInstance -Require 'Microsoft.VisualStudio.Workload.NetWeb' -Version 15.0 -Latest;
 if ($studioInstance) {
     $scriptPath = Join-Path $studioInstance.InstallationPath 'Common7\Tools\VsDevCmd.bat'
     if (Test-Path $scriptPath) {
@@ -23,7 +23,7 @@ if ($studioInstance) {
     }
 }
 
-$www = $env:www
+$www = $env:www;
 
 function Search-AllTextFiles {
     param(
@@ -32,5 +32,5 @@ function Search-AllTextFiles {
         [switch]$SimpleMatch
     );
 
-    Get-ChildItem . * -Recurse -Exclude ('*.dll', '*.pdf', '*.pdb', '*.zip', '*.exe', '*.jpg', '*.gif', '*.png', '*.ico', '*.svg', '*.bmp', '*.tif', '*.tiff', '*.psd', '*.cache', '*.doc', '*.docx', '*.xls', '*.xlsx', '*.dat', '*.mdf', '*.nupkg', '*.snk', '*.ttf', '*.eot', '*.woff', '*.tdf', '*.gen', '*.cfs', '*.map', '*.min.js', '*.data', '*.tis', '*.fdt', '*.pack', 'index', '*.ide', '*.tmp') | Select-String -Pattern:$pattern -SimpleMatch:$SimpleMatch -CaseSensitive:$CaseSensitive
+    Get-ChildItem . * -Recurse -Exclude ('*.dll', '*.pdf', '*.pdb', '*.zip', '*.exe', '*.jpg', '*.gif', '*.png', '*.ico', '*.svg', '*.bmp', '*.tif', '*.tiff', '*.psd', '*.cache', '*.doc', '*.docx', '*.xls', '*.xlsx', '*.dat', '*.mdf', '*.nupkg', '*.snk', '*.ttf', '*.eot', '*.woff', '*.tdf', '*.gen', '*.cfs', '*.map', '*.min.js', '*.data', '*.tis', '*.fdt', '*.pack', 'index', '*.ide', '*.tmp') | Select-String -Pattern:$pattern -SimpleMatch:$SimpleMatch -CaseSensitive:$CaseSensitive;
 }
